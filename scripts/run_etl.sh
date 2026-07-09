3e36abebde64600c7ed94e4e3da21717e100a1c6
Dipayan Lodh <er.dipayanlodh@example.com>
2026-07-09 21:19:03 +0530

    Added the linux

---
+++ scripts/run_etl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "===================================="
+echo "Starting ETL Environment"
+echo "===================================="
+
+docker compose up -d
+
+echo ""
+echo "Docker containers started."
+
+echo ""
+echo "Open Airflow:"
+echo "http://localhost:8080"
+
+echo ""
+echo "Trigger the DAG: customer_etl_pipeline"
